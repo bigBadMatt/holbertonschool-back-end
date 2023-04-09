0f76e05991ff2b84327198a012cb67005b66dbab
bigBadMatt <5707@holbertonstudents.com>
2023-04-09 03:21:58 -0700

    converted py script to output to CSV instad of stdio

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+given employee ID, returns information about his/her TODO list progress
+"""
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+
+    user_id = sys.argv[1]
+    user_url = "https://jsonplaceholder.typicode.com/users/{}"\
+        .format(sys.argv[1])
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+
+    todo_list = requests.get(
+        todo_url,
+        params={"userId": sys.argv[1]}
+    ).json()
+
+    completed_todo = requests.get(
+        todo_url,
+        params={
+            'completed': 'true',
+            'userId': sys.argv[1]
+        }
+    ).json()
+
+    user_name = requests.get(user_url).json().get("name")
+    TOTAL_NUMBER_OF_TASKS = len(todo_list)
+    NUMBER_OF_DONE_TASKS = len(completed_todo)
+
+    with open("{}.csv".format(user_id), mode='w') as csv_file:
+        writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
+
+        for task in todo_list:
+            writer.writerow([
+                user_id,
+                user_name,
+                task['completed'],
+                task['title']
+            ])
